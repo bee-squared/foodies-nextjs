033684b84a167e2ea8e11dfffb8e17542aecd3c0
Brian Bouchard <b.bouch@gmail.com>
2024-02-27 16:00:10 -0700

    Build out meal detail page and style it

--- app/meals/[slug]/page.jsx
+++
@@ -1,9 +0,0 @@
-export default function CommunityPage({ params }) {
-  return (
-    <main>
-      <h1 style={{ color: "white", textAlign: "center", marginTop: "40px" }}>
-        Meals {params.slug} Page
-      </h1>
-    </main>
-  );
-}
